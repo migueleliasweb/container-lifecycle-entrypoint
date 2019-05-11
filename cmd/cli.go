a2054f317773ca679d33a74c727f6109590d737f
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2019-05-11 22:49:44 +1000

    Ported basic cli features to cobra + viper

--- cmd/cli.go
+++ cmd/cli.go
@@ -3,10 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	// jww "github.com/spf13/jwalterweatherman"
 )
 
 //ConfigureCli2 Configures CLI flags
@@ -59,16 +61,33 @@ var rootCmd = &cobra.Command{
 	Long:  "Sends probes according to the underlying cmd lifecycle state",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info("config: ", viper.GetString("config"))
-		log.Info("cmd: ", subCmd)
+		log.Info("cmd: ", viper.GetString("cmd"))
 		log.Info("cmdARGS: ", viper.GetStringSlice("args"))
 	},
 }
 
 func init() {
+	// //very useful to troubleshooting stuff with cobra+viper
+	// jww.SetStdoutThreshold(jww.LevelDebug)
+
 	viper.SetEnvPrefix("CLE")
 	viper.AutomaticEnv()
 
-	//root flags
+	//viper root flags
+	rootCmd.Flags().StringVar(
+		&config,
+		"config",
+		".cle_config",
+		"config file (default is \"./.cle_config.yaml\")",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&subCmd,
+		"cmd",
+		"",
+		"",
+		"Underlying cmd to exec",
+	)
 
 	rootCmd.Flags().StringSliceVarP(
 		&subCmdArgs,
@@ -78,18 +97,27 @@ func init() {
 		"Underlying cmd args",
 	)
 
-	rootCmd.Flags().StringVar(
-		&config,
-		"config",
-		"",
-		"config file (default is \"./.cle.yaml\")",
-	)
-
 	//viper binds
+	viper.BindPFlag("cmd", rootCmd.Flags().Lookup("cmd"))
 	viper.BindPFlag("args", rootCmd.Flags().Lookup("args"))
 	viper.BindPFlag("config", rootCmd.Flags().Lookup("config"))
 
-	fmt.Println("FMT: ", viper.GetString("config"))
+	//setup configuration file
+	viper.AddConfigPath("/")
+	viper.AddConfigPath("/etc/cle")
+	if pwd, err := os.Getwd(); err == nil {
+		viper.AddConfigPath(pwd)
+	}
+	cobra.OnInitialize(func() {
+		configFile := viper.GetString("config")
+		extension := filepath.Ext(configFile)
+		if extension != "" {
+			configFile = configFile[0 : len(configFile)-len(extension)]
+			viper.Set("config", configFile) //fixes the value to future calls
+		}
+		viper.SetConfigName(viper.GetString("config"))
+		viper.ReadInConfig()
+	})
 }
 
 //Run Cobras entrypoint
